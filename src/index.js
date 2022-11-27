5d286649a00f94f50351da48b0886716ea0c518e
lalit <lk2690722@gmail.com>
2022-11-27 17:49:01 +0530

    installed multer packege

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ const route = require('./routes/route')
 const bodyParser = require('body-parser')
 const app = express()
 const mongoose = require('mongoose')
+const multer = require('multer')
 
 app.use(bodyParser.json())
+app.use(multer().any())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 mongoose.connect('mongodb+srv://Lalit:g1b1eD2zYIwUl67Z@cluster0.xmtgwuj.mongodb.net/Project-2', {
@@ -15,6 +17,6 @@ mongoose.connect('mongodb+srv://Lalit:g1b1eD2zYIwUl67Z@cluster0.xmtgwuj.mongodb.
 
 app.use('/', route)
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port', (process.env.PORT || 3000))
+app.listen(process.env.PORT || 3001, function () {
+    console.log('Express app running on port', (process.env.PORT || 3001))
 });
